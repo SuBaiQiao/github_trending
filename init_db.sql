049006f566f07baa504eb01b074ebbd5caaeea4f
SuBaiQiao <1294749811@qq.com>
2025-03-14 20:50:40 +0800

    数据库大小调整

--- init_db.sql
+++ init_db.sql
@@ -5,7 +5,7 @@ USE github_trending;
 CREATE TABLE IF NOT EXISTS trending_repos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
+    description VARCHAR(512),
     author VARCHAR(100) NOT NULL,
     language VARCHAR(50),
     stars VARCHAR(20),
